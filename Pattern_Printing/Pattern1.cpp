5652c9bdfde69c2ed5c6ea69f760291a3bfab0a4
Arshad Rahmani <rahmaniarshad@outlook.com>
2026-04-28 01:10:06 +0530

    Pattern1 commit

---
+++ Pattern_Printing/Pattern1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+  int n = 5;
+  for (int row = 1; row <= 5; row++)
+  {
+    for (int col = 1; col <= 5; col++)
+    {
+      cout << col << " ";
+    }
+    cout << endl;
+  }
+}
