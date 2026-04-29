14058ff2299585b827158a40f7d7bd2b3e7cc988
Arshad Rahmani <rahmaniarshad@outlook.com>
2026-04-29 23:38:53 +0530

    Pattern 19 & 20 commit

---
+++ Pattern_Printing/Pattern19.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout << "Enter Row: ";
+  cin >> n;
+
+  for (int row = 1; row <= n; row++)
+  {
+    // Part 1
+    for (int col = 1; col <= n - row; col++)
+    {
+      cout << " " << " ";
+    }
+    // Part 2
+    for (int i = 1; i <= (2 * row) - 1; i++)
+    {
+      cout << "*" << " ";
+    }
+    cout << endl;
+  }
+}
